21f3674d8e4b4cececf72c245ef4de70c139e666
Anplitova Ekaterina <anplitova@yandex.ru>
2020-03-30 21:59:09 +0400

    Настройка проекта

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+import { Layout } from "~/components/Layout/Layout"
+
+export default class extends Component {
+  render() {
+    return (
+      <Layout>
+        <div>
+          <h1>Index</h1>
+        </div>
+      </Layout>
+    )
+  }
+}
